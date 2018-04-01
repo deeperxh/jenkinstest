572d13980dffbd9422168ecd51ac3c4a6591f9ea
deeperxh <deeperxh@gmail.com>
2018-04-01 19:17:13 +0800

    修改输出

--- src/main/java/com/deeperxh/jenkinsdemo/MainController.java
+++ src/main/java/com/deeperxh/jenkinsdemo/MainController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
         @RequestMapping("/")
         @ResponseBody
         String home() {
-            return "Love KK!\n"+"Love Xiaohan!";
+            return "XH Love KK!\n"+"KK Love Xiaohan!";
         }
 
 
